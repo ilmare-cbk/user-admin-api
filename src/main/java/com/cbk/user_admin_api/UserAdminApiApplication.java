48019786212e857a7f86641a97cd939a76a2b058
ilmare-cbk <cbk104325@gmail.com>
2025-08-14 17:37:21 +0900

    chore 프로젝트 초기 설정

---
+++ src/main/java/com/cbk/user_admin_api/UserAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.cbk.user_admin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbk/user_admin_api/UserAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbk.user_admin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
